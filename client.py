dec3cd3482cd53490dfb6908565f69683106a450
Bhargav Sarvaria <34936854+bhargav-sarvaria@users.noreply.github.com>
2022-09-29 11:21:27 -0700

    Creating logic ring

--- client.py
+++ client.py
@@ -1,87 +1,133 @@
 import socket
 import json
 import sys
-from tkinter import RIGHT
-from turtle import right
-from unicodedata import name
+import threading
 
-HEADER = 64
-SYS_IP = socket.gethostbyname(socket.gethostname())
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = '127.0.0.1'
-SERVER_PORT = 17000
-LEFT_PORT = 17001
-RIGHT_PORT = 17002
+# arg1 -> Port to Send Messages
+# arg2 -> Port to Receive Messages
+# arg3 -> localIp address
+# arg4 -> IP address of the Tracker process
 
-def register(msg, server, port):
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# resp code 1 = Successful, 2 = Server Error, 3 = Bad Request, 4 = Internal Error
+
+bufferSize = 1024
+localIp = sys.argv[3] if len(sys.argv) > 3 else "127.0.0.1"
+serverAddressPort = (sys.argv[4] if len(sys.argv) > 4 else localIp, 17000)
+encoding = 'utf-8'
+# "10.120.70.117"
+LEFT_PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 17001
+RIGHT_PORT = int(sys.argv[2]) if len(sys.argv) > 2 else 17002
+handle = ''
+neighbours = {}
+
+# Create a UDP socket at client side
+UDPClientSocketSend = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPClientSocketRcv = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+UDPClientSocketSend.bind((localIp, LEFT_PORT))
+UDPClientSocketRcv.bind((localIp, RIGHT_PORT))
+
+# Send to server using created UDP socket
+def send_message(req, addr):
+    resp = {}
     try:
-        ADDR = (server, port)
-        conn.bind((SYS_IP, LEFT_PORT))
-        conn.connect(ADDR)
-        conn.sendall(bytes(msg, encoding="utf-8"))
-        resp = json.loads(conn.recv(2048).decode(FORMAT))
-        print(resp['MSG'])
-    finally:
-        conn.close()
+        UDPClientSocketSend.sendto(json.dumps(req).encode(encoding), addr)
+        bytesAddressPair = UDPClientSocketSend.recvfrom(bufferSize)
+        resp = json.loads(bytesAddressPair[0].decode(encoding))
+    except Exception as e:
+        print(e)
+        resp = {'code': 4, 'message' : 'Error while sending request to server'}
+    return resp
 
-def isgoodipv4(s):
-    pieces = s.split('.')
-    if len(pieces) != 4: return False
-    try: return all(0<=int(p)<256 for p in pieces)
-    except ValueError: return False
+def handle_incoming_messages():
+    while(True):
+        try:
+            bytesAddressPair = UDPClientSocketRcv.recvfrom(bufferSize)
+            message = json.loads(bytesAddressPair[0].decode(encoding))
+
+            if message['type'] == 'setup':
+                neighbours[message['handle']] = message['data']
+                resp = {'status': 1, 'type': 'setup'}
+                UDPClientSocketSend.sendto(json.dumps(resp).encode(encoding), (message['rcv_ip'], message['rcv_port']))
+            elif message['type'] == 'propogate_tweet':
+                if message['sender'] == handle:
+                    # End Tweet logic here
+                    return
+
+                print(f"Received message from: {message['sender']}")
+                print(f"@{message['tweeter']}: {message['tweet']}")
+                req = {'type': 'propogate_tweet', 'sender': handle, 'tweeter': message['tweeter'], 'tweet': message['tweet'] }
+                send_to = neighbours[handle]['rightNeighbour']
+                UDPClientSocketSend.sendto(json.dumps(req).encode(encoding), (
+                    send_to['IP'], send_to['RIGHT_PORT']
+                ))
+
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
+    while True:
+        handle = input('Enter a handle name to register:')
+        data = {'handle' : handle, 'IP': localIp, 'LEFT_PORT': LEFT_PORT, 'RIGHT_PORT': RIGHT_PORT, 'followers': []}
+        request_obj = {'type': 'register', 'rcv_ip': localIp, 'rcv_port': LEFT_PORT, 'data': data }
+        resp = send_message(request_obj, serverAddressPort)
+        print(resp['message'])
+        if resp['status'] == 1:
+            break
     
-    # We input the Server IP address from user and 
-    while SERVER == '':
-        SERVER = input('Enter the IP address of Tracker:')
-        if( not isgoodipv4(SERVER)): 
-            SERVER = ''
-            print("[ERROR] Please enter a valid IP address")
-
-    while SERVER_PORT == 0:
-        SERVER_PORT = input('Enter the Server\'s Port Number:')
-        if(not SERVER_PORT.isdigit() or int(SERVER_PORT) not in range(17000,17500)): 
-            SERVER_PORT = 0
-            print("[ERROR] Please enter a valid Port # : Allowed Range: 17000 - 17499")
-        else:
-            SERVER_PORT = int(SERVER_PORT)
-    
-    while LEFT_PORT == 0:
-        LEFT_PORT = input('Enter the Left Port Number:')
-        if(not LEFT_PORT.isdigit() or int(LEFT_PORT) not in range(17000,17500)): 
-            LEFT_PORT = 0
-            print("[ERROR] Please enter a valid Port # : Allowed Range: 17000 - 17499 :")
-        elif socket.gethostbyname(socket.gethostname()) == SERVER and int(LEFT_PORT) == SERVER_PORT:
-            LEFT_PORT = 0
-            print("[ERROR] Please enter a valid Port # : Given Port is already occupied by Server")
-        else:
-            LEFT_PORT = int(LEFT_PORT)
-
-    while RIGHT_PORT == 0:
-        RIGHT_PORT = input('Enter the Right Port Number:')
-        if(not RIGHT_PORT.isdigit() or int(RIGHT_PORT) not in range(17000,17500) or int(RIGHT_PORT) == LEFT_PORT): 
-            RIGHT_PORT = 0
-            print("[ERROR] Please enter a valid Port # : Allowed Range: 17000 - 17499 : Value cannot be equal to Left Port #")
-        elif socket.gethostbyname(socket.gethostname()) == SERVER and int(RIGHT_PORT) == SERVER_PORT:
-            RIGHT_PORT = 0
-            print("[ERROR] Please enter a valid Port # : Given Port is already occupied by Server")
-        else:
-            RIGHT_PORT = int(RIGHT_PORT)
+    threading.Thread(target=handle_incoming_messages).start()
 
     while True:
-        command = input('Enter a command:').split(' ')
+        try:
+            command = input('Enter a command:').split(' ')
+            if command[0] == 'query' and command[1] == 'handles':
+                request_obj = {'type': 'query_handles', 'rcv_ip': localIp, 'rcv_port': LEFT_PORT, 'data': data }
+                resp = send_message(request_obj, serverAddressPort)
 
-        if command[0] == 'register':
-            handle = command[1][1:]
-            msg = {'TYPE': 'register', 'HANDLE' : handle, 'IP': SYS_IP, 'LEFT_PORT': LEFT_PORT, 'RIGHT_PORT': RIGHT_PORT}
-            register(json.dumps(msg), SERVER, SERVER_PORT)
+                if resp['status'] == 1:
+                    print(f"User Count: {resp['count']}")
+                    print(json.dumps(resp['data'], indent=2))
+                else:
+                    print(resp['message'])
+            elif command[0] == 'follow':
+                to_follow = command[1][1:]
+                request_obj = {'type': 'follow', 'rcv_ip': localIp, 'rcv_port': LEFT_PORT, 'handle': handle, 'to_follow': to_follow }
+                resp = send_message(request_obj, serverAddressPort)
+                print(resp['message'])
+            elif command[0] == 'drop':
+                to_drop = command[1][1:]
+                request_obj = {'type': 'drop', 'rcv_ip': localIp, 'rcv_port': LEFT_PORT, 'handle': handle, 'to_drop': to_drop }
+                resp = send_message(request_obj, serverAddressPort)
+                print(resp['message'])
+            elif command[0] == 'exit':
+                print('Exiting the application!')
+                break
+            elif command[0] == 'tweet':
+                tweet = command[1]
+                request_obj = {'type': 'tweet', 'rcv_ip': localIp, 'rcv_port': LEFT_PORT, 'handle': handle}
+                resp = send_message(request_obj, serverAddressPort)
 
-        else:
-            print('Invalid command')
-
-
-    
-
+                if resp['status'] == 1:
+                    followers = resp['followers']
+                    total = len(followers)
+                    if total > 1:
+                        neighbours[handle] = {leftNeighbour, rightNeighbour}
+                        for idx, follower in enumerate(followers[1:]):
+                            leftNeighbour = followers[(idx - 1)%total]
+                            rightNeighbour = followers[(idx + 1)%total]
+                            request_obj = {'type': 'setup', 'rcv_ip': localIp, 'rcv_port': LEFT_PORT, 'handle': handle, 'data': {leftNeighbour, rightNeighbour} }
+                            resp = send_message(request_obj, (follower['IP'], follower['RIGHT_PORT']))
+                            if resp['status'] != 1:
+                                print('Error in creating logical ring!')
+                                break
+                        
+                        req = {'type': 'propogate_tweet', 'sender': handle, 'tweeter': handle, 'tweet': tweet }
+                        send_to = neighbours[handle]['rightNeighbour']
+                        UDPClientSocketSend.sendto(json.dumps(req).encode(encoding), (
+                            send_to['IP'], send_to['RIGHT_PORT']
+                        ))
+                        
+            else:
+                print('Invalid command')
+        except Exception as e:
+            print(e)
--- tracker.py
+++ tracker.py
@@ -1,56 +1,150 @@
-import socket 
+import socket
 import threading
 import json
-import time
+import sys
 
-PORT = 17000
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+# arg1 -> Port for Tracker
+# arg2 -> IP address of Tracker
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+localIP     = sys.argv[2] if len(sys.argv) > 2 else "127.0.0.1"
+localPort   = sys.argv[1] if len(sys.argv) > 1 else 17000
+bufferSize  = 1024
+encoding = 'utf-8'
 
+# Database for storing information of Tweeter users
 USERS = []
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket.bind((localIP, localPort))
+print("UDP server up and listening")
 
-    connected = True
-    while connected:
-        msg = json.loads(conn.recv(1024).decode(FORMAT))
-        if msg:
-            print(f"[{addr}] {msg}")
-            if msg['TYPE'] == 'register':
-                resp = json.dumps(register_user(msg))
-                conn.sendall(bytes(resp, encoding="utf-8"))
-                connected = False
-    conn.close()
+def handle_client(req):
+    if req['type'] == 'register':
+        register_user(req)
+    elif req['type'] == 'query_handles':
+        query_handles(req)
+    elif req['type'] == 'follow':
+        follow(req)
+    elif req['type'] == 'drop':
+        drop(req)
 
-def register_user(msg):
+def tweet(req):
+    try:
+        for user in USERS:
+            if user['handle'] == req['handle']:
+                followers = user['followers']
+                followers.insert(0, user)
+                resp = {'status': 1, 'type': 'tweet', 'count': len(followers), 'followers': followers}
+                UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+                return resp
+        
+        resp = {'status': 3, 'type': 'tweet', 'message': f"Handle {req['handle']} does not exist!"}
+        UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+        return resp
+    except Exception as e:
+        print(e)
+        resp = {'status': 2, 'type': 'follow', 'message': 'Internal error in Tracker'}
+        UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+        return resp
+
+def drop(req):
+    try:
+        for idx, user in enumerate(USERS):
+            if user['handle'] == req['to_drop']:
+                followers = user['followers']
+                if not any(u['handle'] == req['handle'] for u in followers):
+                    resp = {'status': 3, 'type': 'drop', 'message': f"Handle {req['handle']} does not exist in the followers list!"}
+                    UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+                    return
+                followers[:] = [u for u in followers if u.get('handle') != req['handle']]
+                followers = sorted(followers, key=lambda d: d['handle'])
+                user['followers'] = followers
+                USERS[idx] = user
+                print(json.dumps(USERS, indent=2))
+                resp = {'status': 1, 'type': 'drop', 'message': f"{req['handle']} is not following {req['to_drop']} anymore"}
+                UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+                return
+
+        resp = {'status': 3, 'type': 'follow', 'message': f"Handle {req['to_drop']} does not exist!"}
+        UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+        return
+        
+    except Exception as e:
+        print(e)
+        resp = {'status': 2, 'type': 'follow', 'message': 'Internal error in Tracker'}
+        UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+        return
+
+def follow(req):
+    try:
+        for u in USERS:
+            if u['handle'] == req['handle']:
+                for idx, user in enumerate(USERS):
+                    if user['handle'] == req['to_follow']:
+                        followers = user['followers']
+                        followers.append(u)
+                        followers = sorted(followers, key=lambda d: d['handle'])
+                        user['followers'] = followers
+                        USERS[idx] = user
+                        print(json.dumps(USERS, indent=2))
+                        resp = {'status': 1, 'type': 'follow', 'message': f"{req['handle']} is now following {req['to_follow']}"}
+                        UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+                        return
+
+                resp = {'status': 3, 'type': 'follow', 'message': f"Handle {req['to_follow']} does not exist!"}
+                UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+                return
+
+        resp = {'status': 3, 'type': 'follow', 'message': f"Handle {req['handle']} does not exist!"}
+        UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+        return
+        
+    except Exception as e:
+        print(e)
+        resp = {'status': 2, 'type': 'follow', 'message': 'Internal error in Tracker'}
+        UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+        return
+
+# Function to handle registeration of new users
+def register_user(req):
+    data = req['data']
     for user in USERS:
-        if user['HANDLE'] == msg['HANDLE']:
-            return {'STATUS': 2, 'MSG': 'Handle already exists'}
-        if user['IP'] == msg['IP'] and user['LEFT_PORT'] == msg['LEFT_PORT']:
-            return {'STATUS': 2, 'MSG': 'Given host and port combination is already registered'}
-        if user['IP'] == msg['IP'] and user['RIGHT_PORT'] == msg['RIGHT_PORT']:
-            return {'STATUS': 2, 'MSG': 'Given host and port combination is already registered'}
+        if user['handle'] == data['handle']:
+            resp = {'status': 3, 'type': 'register', 'message': 'handle already exists'}
+            UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+            return
+        if user['IP'] == data['IP'] and user['LEFT_PORT'] == data['LEFT_PORT']:
+            resp = {'status': 3, 'type': 'register', 'message': 'Given host and port combination is already registered'}
+            UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+            return
+        if user['IP'] == data['IP'] and user['RIGHT_PORT'] == data['RIGHT_PORT']:
+            resp =  {'status': 3, 'type': 'register', 'message': 'Given host and port combination is already registered'}
+            UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+            return
 
-    USERS.append(msg)
-    return {'STATUS': 1, 'MSG': 'Handle registered successfully'}
+    USERS.append(data)
+    resp = {'status': 1, 'type': 'register', 'message': 'handle registered successfully'}  
+    UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
 
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}:{PORT}")
-    while True:
+# Function to return the count and list of registered users
+def query_handles(req):
+    resp = {'status': 1, 'type': 'query_handles', 'count': len(USERS), 'data': USERS}  
+    UDPServerSocket.sendto(json.dumps(resp).encode(encoding), (req['rcv_ip'], req['rcv_port']))
+    return resp
 
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        time.sleep(20)
-        # print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-
-
-print("[STARTING] server is starting...")
-start()
+# Listen for incoming datagrams
+while(True):
+    try:
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+        message = json.loads(bytesAddressPair[0].decode(encoding))
+        address = bytesAddressPair[1]
+        if message['type'] == 'tweet':
+            resp = tweet(message)
+            if resp['status'] == 1 and len(resp['followers']) > 1:
+                bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+                message = json.loads(bytesAddressPair[0].decode(encoding))
+        else:
+            threading.Thread(target=handle_client, args=(message,)).start()
+    except Exception as e:
+        print(e)
